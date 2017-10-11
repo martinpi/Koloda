8ccb702956907ce6e79695647d821a0e0719cb63
Martin Pichlmair <pi@brokenrul.es>
2017-10-11 22:15:47 +0200

    - 10 cards designed

--- Example/Koloda/BackgroundAnimationViewController.swift
+++ Example/Koloda/BackgroundAnimationViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 import Koloda
 import pop
 
-private let numberOfCards: Int = 5
+private let numberOfCards: Int = 10
 private let frameAnimationSpringBounciness: CGFloat = 9
 private let frameAnimationSpringSpeed: CGFloat = 16
 private let kolodaCountOfVisibleCards = 2
 private let kolodaAlphaValueSemiTransparent: CGFloat = 0.1
 
 private let cardTransitions: Dictionary = [ 0 : [ 1, 2 ],
-                                            1 : [ 0, 2 ],
-                                            2 : [ 0, 1 ],
-                                            3 : [ 0, 1 ],
-                                            4 : [ 0, 1 ] ]
+                                            1 : [ 2, 3 ],
+                                            2 : [ 3, 4 ],
+                                            3 : [ 4, 0 ],
+                                            4 : [ 5, 0 ],
+                                            5 : [ 6, 0 ],
+                                            6 : [ 7, 0 ],
+                                            7 : [ 8, 0 ],
+                                            8 : [ 9, 0 ],
+                                            9 : [ 0, 1 ] ]
 
 private var nextCard: Int = 0
 
